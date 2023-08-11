40ebab05a99384bfc7696dc85f70dc881f4a7fc9
stephniashvili <stephniashvilinatia@gmail.com>
2023-08-11 02:24:12 -0700

    first commit

---
+++ day 1/lession1.py
@@ -0,0 +1,15 @@
+name= "natia"
+
+surname= "stephniashvili"
+
+age= 32
+
+height= 160
+
+interested_in_programming = True
+
+
+lazy = False
+
+
+print("my name is"+" "+name+" "+"and my surname is"+" "+surname+" "+"i am"+" "+str(age)+ " "+"years old"+" "+"my height is"+" "+str(height))
